c19a321f608932fc5d51f159ed503904856faeb0
Boudalia Bediss <bediss.boudalia@volvo.com>
2019-12-18 15:09:05 +0100

    implement update

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import { app, BrowserWindow } from 'electron';
 
+require('update-electron-app')()
+
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
   app.quit();
